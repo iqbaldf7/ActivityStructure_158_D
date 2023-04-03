f7ee54634ad73c72e79c39c5638245a91ae60a07
iqbaldf7 <iqbalfathonah10@gmail.com>
2023-04-03 14:46:45 +0700

    mengetahui identitas mahasiswa

--- ActivityStructure_158_D/ActivityStructure_158_D.cpp
+++ ActivityStructure_158_D/ActivityStructure_158_D.cpp
@@ -18,6 +18,16 @@ int main()
 	mhs1.alamat = "subang";
 	mhs1.umur 22;
 
+	cout << "Masukan nim: ";
+	cin >> mhs2.nim;
+	cout <<"Masukan nama: ";
+	cin >> mhs2.nama;
+	cout >> "Masukan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukan umur: ";
+	cin >> mhs2.umur;
+
+
 
 
 

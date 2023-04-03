bd345885bdacaeb6b13558b4e66c70ed2acd6f94
iqbaldf7 <iqbalfathonah10@gmail.com>
2023-04-03 14:41:04 +0700

    mengetaui identitas

--- ActivityStructure_158_D/ActivityStructure_158_D.cpp
+++ ActivityStructure_158_D/ActivityStructure_158_D.cpp
@@ -8,3 +8,16 @@ struct Mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20220140188";
+	mhs1.nama = "Joko";
+	mhs1.alamat = "subang";
+	mhs1.umur 22;
+
+
+
+

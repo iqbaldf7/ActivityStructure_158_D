2336859cf4c4612309ceade440ffc547162984ae
iqbaldf7 <iqbalfathonah10@gmail.com>
2023-04-03 15:43:32 +0700

    untuk menentukan sebuah identitas mahasiswa

--- ActivityStructure3_158_D/ActivityStructure3_158_D.cpp
+++ ActivityStructure3_158_D/ActivityStructure3_158_D.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
-
 using namespace std;
 
 struct AlamatDetail
 {
-	string desa[20];
-	string kota[20];
+	char desa[20];
+	char kota[20];
 };
 
-struct Mahasiswa {
-	string nim[12];
-	string nama[20];
+struct Mahasiswa
+{
+	char NIM[12];
+	char nama[25];
 	AlamatDetail alamat;
 	int umur;
 };
-int main() {
 
+
+int main() {
 	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukkan NIM :";
+		cin.getline(mhs[i].NIM, 12);
+		cout << "Masukkan Nama :";
+		cin.getline(mhs[i].nama, 25);
+		cout << "Alamat :";
+		cout << "\n\tMasukkan Desa :";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "\tMasukkan Kota :";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukkan Umur :";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
 
 	for (int i = 0; i < 3; i++)
 	{
-		cout << "Masukkan nim: ";
+		cout << "\n\nNIM = " << mhs[i].NIM;
+		cout << "\nNama = " << mhs[i].nama;
+		cout << "\nDesa = " << mhs[i].alamat.desa;
+		cout << "\nKota = " << mhs[i].alamat.kota;
+		cout << "\nUmur = " << mhs[i].umur;
 	}
+}
